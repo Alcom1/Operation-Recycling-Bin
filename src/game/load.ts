8f5caac20a11d28b3b78e71050b195aecd30045c
Alex Bronner <acbronn@gmail.com>
2021-11-15 19:04:53 -0500

    [Build] Hiding levels that aren't select jump levels

--- src/game/load.ts
+++ src/game/load.ts
@@ -31,7 +31,7 @@ window.onload = function() {
         canvas, 
         "assets/scenes/",
         "scenes",
-        ["LevelInterface", "LEVEL_18"],
+        ["LevelInterface", "LEVEL_16"],
         [
             BrickHandler,
             BrickHandlerDebug,
