bf578181b929e7be8cfbc54ab57a6ea2a610e394
Alex Bronner <acbronn@gmail.com>
2023-07-25 22:39:00 -0400

    Fixed button tiles.

--- src/game/gameobjects/bricktilebutton.ts
+++ src/game/gameobjects/bricktilebutton.ts
@@ -1,3 +1,4 @@
+import { Collision } from "engine/gameobjects/gameobject";
 import { Collider } from "engine/modules/collision";
 import { MASKS } from "engine/utilities/math";
 import BrickTile, { BrickTileParams } from "./bricktile";
@@ -13,7 +14,6 @@ export default class BrickTileButton extends BrickTile {
 
     plates: BrickTile[] = [];
     private isLock : boolean = false;   // If this button is locked and can't be flipped again
-    private isLeft : boolean = false;   // If the presser of this button left and it *can* be flipped again
 
     /** Constructor */
     constructor(params: BrickTileParams) {
@@ -29,20 +29,6 @@ export default class BrickTileButton extends BrickTile {
             "Level") as BrickTile[]).filter(p => p.circuit == this.circuit);
     }
 
-    /** Update timer */
-    public update(dt: number) {
-
-        // Unlock the button after the presser leaves
-        if (this.isLeft) {
-            this.isLock = false;
-        }
-
-        // Setup a check for if the presser left while this button is locked
-        if (this.isLock) {
-            this.isLeft = true;
-        }
-    }
-
     /** Get hazard and passive colliders of this brick. */
     public getColliders() : Collider[] {
 
@@ -53,20 +39,29 @@ export default class BrickTileButton extends BrickTile {
             max : this.gpos.getAdd({ x : this.width, y :  0}) 
         }]);
     }
+
+    /** Override collision check  */
+    protected resolveCollisions(collisions : Collision[]) {
+        super.resolveCollisions(collisions)
+
+        //Unlock this button if it isn't colliding with anything anymore.
+        if(collisions.length == 0) {
+
+            this.isLock = false;
+        }
+    }
     
-    /** Flip */
+    /** Flip on/off when pressed */
     public resolveCollision(mask : number) {
 
         // Turn off
         if (mask & MASKS.press) {
 
-            // Left check failed, presser is still here
-            this.isLeft = false;
+            if(!this.isLock) {
 
-            if (!this.isLock) {
-                var temp = !this.isOn;                      // Store opposite of this button
-                this.plates.forEach(p => p.setOnOff(temp)); // Set all other plates in this circuit
-                this.isLock = true;                         // Lock the button to prevent repeat presses
+                this.setOnOff(!this.isOn);                          // Store opposite of this button
+                this.plates.forEach(p => p.setOnOff(this.isOn));    // Set all other plates in this circuit
+                this.isLock = true;                                 // Lock the button to prevent repeat presses
             }
         }
     }
