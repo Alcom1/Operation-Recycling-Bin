022e1e907ebacd42975c9fceb437136422b260cb
Alex Bronner <acbronn@gmail.com>
2024-03-20 19:59:48 -0400

    zap.svg

--- src/game/gameobjects/characterbot.ts
+++ src/game/gameobjects/characterbot.ts
@@ -195,7 +195,7 @@ export default class CharacterBot extends Character {
 
         this.partZap = this.parent.pushGO(new Anim({
             ...params,
-            images : [{ name : "zap" }],
+            images : [{ name : "zap", extension : "svg" }],
             speed : 1,
             frameCount : 10,
             zIndex : 50000,
